ebf5bc7fc917d5cb9e57f547d61c01a2a09557d9
aghiwardani <48787322+aghiwardani@users.noreply.github.com>
2020-09-11 09:42:34 +0700

    tugas 14

---
+++ Tugas-14/database/migrations/2020_09_10_134258_create_profile_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfileTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profile', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('email', 100);
+            $table->string('foto', 100);
+            $table->primary('id');
+            $table->unique('id');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profile');
+    }
+}
